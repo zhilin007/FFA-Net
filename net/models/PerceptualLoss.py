2299fc22afad14041120674fdc11b45bba234a86
wangzhilin <1032570740@qq.com>
2020-01-03 16:02:01 +0800

    add perceptualloss

---
+++ net/models/PerceptualLoss.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn.functional as F
+
+
+# --- Perceptual loss network  --- #
+class LossNetwork(torch.nn.Module):
+    def __init__(self, vgg_model):
+        super(LossNetwork, self).__init__()
+        self.vgg_layers = vgg_model
+        self.layer_name_mapping = {
+            '3': "relu1_2",
+            '8': "relu2_2",
+            '15': "relu3_3"
+        }
+
+    def output_features(self, x):
+        output = {}
+        for name, module in self.vgg_layers._modules.items():
+            x = module(x)
+            if name in self.layer_name_mapping:
+                output[self.layer_name_mapping[name]] = x
+        return list(output.values())
+
+    def forward(self, dehaze, gt):
+        loss = []
+        dehaze_features = self.output_features(dehaze)
+        gt_features = self.output_features(gt)
+        for dehaze_feature, gt_feature in zip(dehaze_features, gt_features):
+            loss.append(F.mse_loss(dehaze_feature, gt_feature))
+
+        return sum(loss)/len(loss)
--- net/models/__init__.py
+++ net/models/__init__.py
@@ -2,3 +2,4 @@ import sys,os
 dir=os.path.abspath(os.path.dirname(__file__))
 sys.path.append(dir)
 from FFA import FFA
+from PerceptualLoss import LossNetwork as PerLoss
